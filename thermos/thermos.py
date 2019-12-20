594989b45a8ab9f91b517592340bcc197037cdb7
icyc0re <tiziano.signo@gmail.com>
2019-12-20 21:16:18 +0100

    Add simplified temporary storage of bookmarks, and logging of add operation

--- thermos/thermos.py
+++ thermos/thermos.py
@@ -6,13 +6,27 @@ from logging import DEBUG
 app = Flask(__name__)
 app.logger.setLevel(DEBUG)
 
+bookmarks = []
+
+def store_bookmark(url):
+    bookmarks.append({
+        'url': url,
+        'user': 'tiziano',
+        'date': datetime.now()
+    })
+
 @app.route('/')
 @app.route('/index')
 def index():
     return render_template('index.html')
 
-@app.route('/add')
+@app.route('/add', methods = ['GET', 'POST'])
 def add():
+    if request.method == 'POST':
+        url = request.form['url']
+        store_bookmark(url)
+        app.logger.debug('Stored url: {}'.format(url))
+        return redirect(url_for('index'))
     return render_template('add.html', pagename='add')
 
 @app.errorhandler(404)
